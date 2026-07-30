bf14d58a9ab8db9fb1e44c9a50e9704f95994aa2
Matteo7034 <matteo7034@gmail.com>
2026-07-30 17:57:05 +0200

    Shader stage creation modified createGraphicsPipeline function

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -18,6 +18,21 @@ namespace PipeLine{
         auto fragShaderCode = readFile("shaders/frag.spv");
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode,device);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode,device);
+        //vertShaderInfo
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertShaderStageInfo.module = vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+        //fragShaderInfo
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragShaderStageInfo.module = fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+
+        VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
         vkDestroyShaderModule(device,fragShaderModule,nullptr);
         vkDestroyShaderModule(device,vertShaderModule,nullptr);
     }
